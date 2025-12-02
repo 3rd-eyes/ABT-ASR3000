800541c6598d2f4a092946f6bb5d840d100bed0a
3rd-eyes <167307560+3rd-eyes@users.noreply.github.com>
2025-12-03 00:07:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,11 +11,11 @@
 #
 
 # Modify login IP
-# sed -i 's/192.168.6.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # Modify default SSID
-# sed -i "s/ImmortalWrt-2.4G/Breakwa11/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
-# sed -i "s/ImmortalWrt-5G/Breakwa11 ax/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/ImmortalWrt-2.4G/asr3000-2.4G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/ImmortalWrt-5G/ASR3000-5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 # Modify ppp-down, add sleep 3. my source code is change, no need this
 # sed -i '$a\\sleep 3' package/network/services/ppp/files/lib/netifd/ppp-down
