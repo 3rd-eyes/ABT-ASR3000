bc9d0868d5de2ed8c646a7f7362d2aac66a5604c
3rd-eyes <167307560+3rd-eyes@users.noreply.github.com>
2025-12-26 23:29:34 +0800

    Update opkg URL and set custom firmware version
    Add custom firmware version name and modify opkg URL

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -22,3 +22,6 @@ sed -i "s/ImmortalWrt-5G/ASR3000-5G/g" package/mtk/applications/mtwifi-cfg/files
 
 # Insert two lines before the last line in 99-default-settings-chinese. For Modify opkg url, change mt7981 to filogic, del lines inlude passwall.
 sed -i '/^exit 0$/i sed -i "s,mt7981,filogic,g" "package/base-files/files/etc/opkg/distfeeds.conf"\nsed -i '\''/passwall/d'\'' "package/base-files/files/etc/opkg/distfeeds.conf"' package/emortal/default-settings/files/99-default-settings-chinese
+
+# 固件版本名称自定义
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt By 3rd-eyes $(date +"%Y%m%d") '/g" package/base-files/files/etc/openwrt_release
